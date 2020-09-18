2b118155b2ccc9aa58ccf300ce9d89c6a1b4af79
anthonymjimenez <anthonyjim96@gmail.com>
2020-09-18 15:23:12 -0400

    done

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+    attr_accessor :title
+    def initialize(title)
+        @title = title
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers
+        backers = ProjectBacker.all.map { |kickStart | 
+        if kickStart.project == self
+            kickStart.backer
+        end
+        }
+        backers.compact
+    end
+end
